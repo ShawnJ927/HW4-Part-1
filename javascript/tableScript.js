2f4d94db238c77d3e2ab18e3e6f1b90c50b1602f
ShawnJ927 <shawn.jordan927@gmail.com>
2023-12-03 22:57:40 -0500

    part 1

--- javascript/tableScript.js
+++ javascript/tableScript.js
@@ -5,124 +5,101 @@
     Copyright (c) 2023 by Shawn. 
     All rights reserved.
     Modified Assignment by Shawn Jordan
-    updated by SJ on Nov 14, 2023 at 2:59 PM 
+    updated by SJ on Dec 3, 2023 at 2:59 PM 
 */
 
-// function for generate button to create table
-function generateTable() {
-    // get values from form
-    var startRow = parseInt(document.getElementById("startRow").value); // row min
-    var endRow = parseInt(document.getElementById("endRow").value); // row max
-    var startCol = parseInt(document.getElementById("startCol").value); // col min
-    var endCol = parseInt(document.getElementById("endCol").value); // col max
+// Jquery document ready function
+$(document).ready(function() {
 
-    // check if any input is not valid, then display error messages and return
-    if (isNaN(startRow) || isNaN(endRow) || isNaN(startCol) || isNaN(endCol) || startRow > endRow
-        || startRow < -50 || startRow > 50 || endRow < -50 || endRow > 50 || startCol > endCol || 
-        startCol < -50 || startCol > 50 || endCol < -50 || endCol > 50) {
+    // Jquery for generate button to create table
+    $("#generateTable").click(function() {
+        // get values from form
+        var startRow = parseInt($("#startRow").val()); // row min
+        var endRow = parseInt($("#endRow").val()); // row max
+        var startCol = parseInt($("#startCol").val()); // col min
+        var endCol = parseInt($("#endCol").val()); // col max
 
-        // check which inputs are not a number and display error message then return
-        if(isNaN(startRow)) {
-        document.getElementById('startRowError').textContent = 'number entry must not be empty';
+        // check if any input is not valid, then display error messages and return
+        // using math.abs to check if the values are within range this time
+        if (!$.isNumeric(startRow) || !$.isNumeric(endRow) || !$.isNumeric(startCol) || !$.isNumeric(endCol) ||
+        startRow > endRow || Math.abs(startRow) > 50 || Math.abs(endRow) > 50 ||
+        startCol > endCol || Math.abs(startCol) > 50 || Math.abs(endCol) > 50) {
+
+            // check which inputs are not a number and display error message then return
+            if(!$.isNumeric(startRow)) {
+                $("#startRowError").text('Number entry must not be empty');
+            }
+            if(!$.isNumeric(endRow)) {
+                $("#endRowError").text('Number entry must not be empty');
+            }
+            if(!$.isNumeric(startCol)) {
+                $("#startColError").text('Number entry must not be empty');
+            }
+            if(!$.isNumeric(endCol)) {
+                $("#endColError").text('Number entry must not be empty');
+            }
+
+            // check if row values are valid with jquery
+            if (startRow > endRow) {
+                $("#startRowError").text('Starting number cannot be more than ending number');
+            }
+
+            // check which inputs are not within range
+            if (Math.abs(startRow) > 50) {
+                $("#startRowError").text('Starting and ending numbers must be between -50 and 50');
+            }
+            if (Math.abs(endRow) > 50) {
+                $("#endRowError").text('Starting and ending numbers must be between -50 and 50');
+            }
+
+            // check if col values are valid
+            if (startCol > endCol) {
+                $("#startColError").text('Starting number cannot be more than ending number');
+            }
+            // check if col values are in range
+            if (Math.abs(startCol) > 50) {
+                $("#startColError").text('Starting and ending numbers must be between -50 and 50');
+            }
+            if (Math.abs(endCol) > 50) {
+                $("#endColError").text('Starting and ending numbers must be between -50 and 50');
+            }
+
+            return;
         }
-        if(isNaN(startCol)) {
-        document.getElementById('startColError').textContent = 'number entry must not be empty';
-        }
-        if(isNaN(endRow)) {
-        document.getElementById('endRowError').textContent = 'number entry must not be empty';
-        }
-        if(isNaN(endCol)) {
-        document.getElementById('endColError').textContent = 'number entry must not be empty';
-        }
 
-        // check if row values are valid
-        if (startRow > endRow) {
-            document.getElementById('startRowError').textContent = 'Starting number cannot be more than ending number';
+        // generate table and display
+        var table = '<table class="table-style"><thead><tr><th></th>'; // create table with css and header to start the table
+        
+        // loop to fill the table columns
+        for (var i = startCol; i <= endCol; i++) {
+            table += '<th>' + i + '</th>'; // add col header
         }
 
-        // check which inputs are not within range
-        if (startRow < -50 || startRow > 50) {
-            document.getElementById('startRowError').textContent = 'Starting and ending numbers must be between -50 and 50';
-        }
-        if (endRow < -50 || endRow > 50) {
-        document.getElementById('endRowError').textContent = 'Starting and ending numbers must be between -50 and 50';
-        }
+        table += '</tr></thead><tbody>'; // add to the body of the table
 
-        // check if col values are valid
-        if (startCol > endCol) {
-            document.getElementById('startColError').textContent = 'Starting number cannot be more than ending number';
+        // loop to do the multiplication and fill the table
+        for (var i = startRow; i <= endRow; i++) {
+            table += '<tr><th>'+ i + '</th>'; // add row
+            for (var j = startCol; j <= endCol; j++) {
+                table += '<td>' + i * j + '</td>'; // add col
+            }
+            table += '</tr>'; // end row
         }
 
-        // check if col values are in range
-        if (startCol < -50 || startCol > 50) {
-            document.getElementById('startColError').textContent = 'Starting and ending numbers must be between -50 and 50';
-        }
-        if (endCol < -50 || endCol > 50) {
-        document.getElementById('endColError').textContent = 'Starting and ending numbers must be between -50 and 50';
-        }
+        table += '</tbody></table>'; // end table
+        
+        // check the table for contents
+        console.log('tableContents',table);
 
-        return;
-    }
+        // display the new table to the screen
+        $("#tableContainer").html(table);
 
-    // generate table and display
-    var table = '<table class="table-style"><thead><tr><th></th>'; // create table with css and header to start the table
-    
-    // loop to fill the table columns
-    for (var i = startCol; i <= endCol; i++) {
-        table += '<th>' + i + '</th>'; // add col header
-    }
+        // check to see if table is generated via console
+        console.log("Table should be generated");
+    });   
 
-    table += '</tr></thead><tbody>'; // add to the body of the table
-
-    // loop to do the multiplication and fill the table
-    for (var i = startRow; i <= endRow; i++) {
-        table += '<tr><th>'+ i + '</th>'; // add row
-        for (var j = startCol; j <= endCol; j++) {
-            table += '<td>' + i * j + '</td>'; // add col
-        }
-        table += '</tr>'; // end row
-    }
-
-    table += '</tbody></table>'; // end table
-    
-    // check the table for contents
-    console.log('tableContents',table);
-
-    // display the new table to the screen
-    document.getElementById("tableContainer").innerHTML = table;
-
-    // check to see if table is generated via console
-    console.log("Table should be generated");
-}   
-
-    // add event listener to generate button and have it call the generateTable function
-    document.getElementById("generateTable").addEventListener("click", generateTable);
-
-    /* Clear the error messages when the input textbox's text is changed using listeners */
-    document.getElementById('startRow').addEventListener('input', function() {
-        document.getElementById('startRowError').textContent = '';
-        document.getElementById('startColError').textContent = '';
-        document.getElementById('endRowError').textContent = '';
-        document.getElementById('endColError').textContent = '';  
+    // Jquery to clear the error messages when the input textbox's text is changed using listeners
+    $("#startRow, #endRow, #startCol, #endCol").on("input", function () {
+        $(".error").text('');
     });
-    /* Clear the error messages when the input textbox's text is changed using listeners */
-    document.getElementById('endRow').addEventListener('input', function() {
-        document.getElementById('startRowError').textContent = '';
-        document.getElementById('startColError').textContent = ''; 
-        document.getElementById('endRowError').textContent = '';
-        document.getElementById('endColError').textContent = '';
-    });
-    /* Clear the error messages when the input textbox's text is changed using listeners */
-    document.getElementById('startCol').addEventListener('input', function() {
-        document.getElementById('startRowError').textContent = '';
-        document.getElementById('startColError').textContent = '';
-        document.getElementById('endRowError').textContent = '';
-        document.getElementById('endColError').textContent = '';
-    });
-    /* Clear the error messages when the input textbox's text is changed using listeners */
-    document.getElementById('endCol').addEventListener('input', function() {
-        document.getElementById('startRowError').textContent = '';
-        document.getElementById('startColError').textContent = '';
-        document.getElementById('endRowError').textContent = '';
-        document.getElementById('endColError').textContent = '';
-    });
+});
--- readme.txt
+++
@@ -1,5 +0,0 @@
-Webpage
-https://shawnj927.github.io/GUI-HW3/
-
-Github Link
-https://github.com/ShawnJ927/GUI-HW3
